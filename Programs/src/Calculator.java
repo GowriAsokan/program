c65917184eba83c8083175f9208a7e8a04de99b0
gowri <gowri@full134-air.full.creative>
2018-01-23 16:58:37 +0530

    Two program per day

---
+++ Programs/src/Calculator.java
@@ -0,0 +1,28 @@
+ abstract class GenericCalculator<T>
+ 	{
+	 abstract public T add(T num1, T num2);
+	 abstract public T subtract(T num1, T num2);
+ 	 abstract public T multiply(T num1, T num2);
+	 abstract public T divide(T num1, T num2);
+ 	}
+
+
+public class Calculator extends GenericCalculator<Number>
+	{
+		public Number add(Number num1, Number num2)
+		{
+			return(num1.doubleValue() + num2.doubleValue());
+		}
+		public Number subtract(Number num1, Number num2)
+		{
+			return(num1.doubleValue() - num2.doubleValue());
+		}
+		public Number multiply(Number num1, Number num2)
+		{
+			return(num1.doubleValue() * num2.doubleValue());
+		}
+		public Number divide(Number num1, Number num2)
+	 	{
+			return(num1.doubleValue() / num2.doubleValue());
+	 	}
+	}
---
+++ Programs/src/CalculatorProgramusingGenerics.java
@@ -0,0 +1,19 @@
+
+public class CalculatorProgramusingGenerics
+	{
+	public static void main(String[] args)
+		{
+			Calculator c = new Calculator();
+					
+				System.out.println(c.add(2, 4));
+		
+				System.out.println(c.subtract(40,20.00));
+				
+				System.out.println(c.multiply(3.0,4.0));
+				
+				System.out.println(c.divide(32.0, 4));
+				
+				
+	}
+
+}
